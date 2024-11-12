c84836e459b5883f8912943e9f020becc4256bc3
DowellHd <dstandley@yahoo.com>
2024-11-12 12:34:12 -0800

    Refactor `add` function to use unnamed parameters

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+//func add(num1: Int, num2: Int) -> Int{
+//    return num1 + num2
+//}
+//
+//add(num1: 3,num2: 5)
+////add(num1: 2,num2: 2)
+//
+//var name = "DJ"
+
+func add(_ num1: Int,_ num2: Int) -> Int{
+    return num1 + num2
+}
+
+add(3, 5)
+//add(num1: 2,num2: 2)
+
+var name = "DJ"
