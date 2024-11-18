22c2bce1face43c40e3ac12152a37c49f00a2495
DowellHd <dstandley@yahoo.com>
2024-11-18 12:33:20 -0800

    Cleaned up code

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,9 +1,24 @@
-import UIKit
+import Foundation
 
-//Variable that point to a function
-let add: (Int, Int) -> Int = {
-    (lhs: Int, rhs: Int) -> Int in
+       //(lhs: Int, rhs: Int) -> Int in
+      //or
+let add: (Int, Int) -> Int = { (lhs, rhs) -> Int in
     return  rhs + lhs
 }
 
+/*let result = add(30, 50){ (lhs: Int, rhs: Int) -> Int in
+    lhs + rhs
+}*/
+
+//or
+
+let result = add(10, 15)
+print(result)
+
 print(add(5,5))
+print(result)
+
+//Doesn't work?
+/*add(20, 30) {
+    $0 + $1
+}*/
